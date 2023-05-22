63cb4770263156547bfc8b80eef54a35aeeebbba
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2023-05-22 12:13:23 -0300

    render client  url

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(
   cors({
-    origin: ["https://orientalfisiodf.vercel.app"],
+    origin: ["https://orientalfisiodf.onrender.com"],
   })
 );
 
